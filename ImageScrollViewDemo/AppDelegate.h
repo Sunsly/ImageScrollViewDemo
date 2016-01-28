229c88b0045753b1c0a782cc9e3ae62ec70664e8
辉 <hui@huidemac-mini.local>
2016-01-28 10:13:42 +0800

    Initial Commit

---
+++ ImageScrollViewDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ImageScrollViewDemo
+//
+//  Created by 辉 on 16/1/28.
+//  Copyright © 2016年 辉. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ImageScrollViewDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ImageScrollViewDemo
+//
+//  Created by 辉 on 16/1/28.
+//  Copyright © 2016年 辉. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
